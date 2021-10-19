90f0c6e3f042297cc0a813e6aee61440ec30ae58
mAtencio27 <mAtencio27@hotmail.com>
2021-10-19 14:14:59 +0900

    using both knex one to migrate

--- seeds/createfoods.js
+++ seeds/createfoods.js
@@ -3,25 +3,29 @@ const fs = require("fs");
 const db = require("../server/knex.js");
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  // return knex('table_name').del()
-  //   .then(function () {
-  //     // Inserts seed entries
-  //     return knex('table_name').insert([
-  //       {id: 1, colName: 'rowValue1'},
-  //       {id: 2, colName: 'rowValue2'},
-  //       {id: 3, colName: 'rowValue3'}
-  //     ]);
-  //   });
+  //Deletes ALL existing entries
+  const allFoods = getfoods();
+  return knex('food').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('food').insert(allFoods);
+    });
 };
+
+//console.log(getfoods())
 
 function getfoods() {
   const foods = JSON.parse(fs.readFileSync("./data/foods.json"));
-  const ret = [10]
-  for(let food of foods){
-    ret.push(food.name)
+  const ret = []
+  for(const foodData of foods){
+    const foodName = foodData.name
+    const daysfresh = foodData.daysgood
+    ret.push({"foodName" : foodName, "daysfresh": daysfresh})
   }
-  console.log(ret)
+  //const ret = []
+  // for(let food of foods){
+  //   ret.push(food)
+  // }
+  //console.log(ret)
+  return ret
 }
-
-getfoods()
