bf5ee9841d3b826662c7a875784549eeb676fbab
mAtencio27 <mAtencio27@hotmail.com>
2021-10-19 14:18:01 +0900

    trying to push the migration and seed into heroku

--- server/index.js
+++ server/index.js
@@ -4,20 +4,20 @@ const db = require("./knex");
 
 const PORT = process.env.PORT || 9000;
 
-// (async () => {
-//   try {
-//     console.log("Running migrations....");
-//     await db.migrate.latest();
+(async () => {
+  try {
+    console.log("Running migrations....");
+    await db.migrate.latest();
 
-//     console.log("Starting express...");
-//     app.listen(PORT, () => {
-//       console.log(`App listening on port ${PORT}!`);
-//     });
-//   } catch (err) {
-//     console.error("Error starting app!", err);
-//     process.exit(-1);
-//   }
-// })();
+    console.log("Starting express...");
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  } catch (err) {
+    console.error("Error starting app!", err);
+    process.exit(-1);
+  }
+})();
 
 app.listen(PORT, () => {
   console.log(`App listening on port${PORT}!`);
